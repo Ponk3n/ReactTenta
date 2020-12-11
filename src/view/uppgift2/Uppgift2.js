7e3610c3f23e8fa3c7859016407a8d055a0b7e49
Robert <thesouse@gmail.com>
2020-12-11 11:13:10 +0100

    Add gjort klart uppgift2

--- src/view/uppgift2/Uppgift2.js
+++ src/view/uppgift2/Uppgift2.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Uppgift2 = () => {
+	const [friVilja, setFriVilja] = useState('')
+
 	return (
 		<div>
 			<h1>Uppgift 2</h1>
-			<h2>(värdet av ditt state skall synas i denna tag)</h2>
-			<input placeholder="Denna input skall uppdatera värdet som matas in till ditt state" />
+			<h2>{friVilja}</h2>
+			<input
+				onChange={(event) => setFriVilja(event.target.value)}
+				placeholder="Denna input skall uppdatera värdet som matas in till ditt state"
+			/>
 		</div>
 	)
 }
