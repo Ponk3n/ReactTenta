a323d967399d0fb433186e0ebeb7192e5825f1d7
Robert <thesouse@gmail.com>
2020-12-11 11:51:27 +0100

    Add gjort klart uppgift 3

--- src/view/uppgift3/Uppgift3.js
+++ src/view/uppgift3/Uppgift3.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import StarwarsService from '../../shared/api/service/StarwarsService'
 
 export const Uppgift3 = () => {
-	const character = undefined
-	const count = 1
+	const [character, setCharacter] = useState()
+	const [count, setCount] = useState(1)
 
 	const getCharacterNameFromStarwarsAPI = async () => {
-		const { data } = await StarwarsService.getStarwarsCharacter(count)
+		try {
+			const response = await StarwarsService.getStarwarsCharacter(count)
+			setCharacter(response.data)
+		}
+		catch (error) {
+			console.log(error)
+		}
+
 	}
 
 	const buttons = () => {
 		return <div>
-			<button>Increment</button> <br />
-			<button>Decrement</button>
+			<h2>{count}</h2>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
+			<br />
+			<button onClick={() => setCount(count - 1)}>Decrement</button>
 		</div>
 	}
 
@@ -23,6 +32,10 @@ export const Uppgift3 = () => {
 			</div>
 		}
 	}
+
+	useEffect(() => {
+		getCharacterNameFromStarwarsAPI()
+	}, [count])
 
 	return (
 		<div>
@@ -41,21 +54,21 @@ Denna vy skall använda sig av useEffect tillsammans med useState för att hämt
 	1. Du hittar två variabler i denna vy, character & count, dessa variabler skall bytas ut mot varsitt state.
 	(variabelnamnen skall fortsatt heta likadant och de tidigare variabeldeklarationerna kan tas bort)
 
-	2. ditt count state skall ha initialvärdet 1 
+	2. ditt count state skall ha initialvärdet 1
 	och ditt character initialvärde skall vara en tom array
 
-	3. i funktionen getCharacterNameFromStarwarsAPI() skall du spara 
+	3. i funktionen getCharacterNameFromStarwarsAPI() skall du spara
 	värdet av data variabeln i ditt character state
 
-	4. I funktionen buttons() hittar du två button taggar. Skriv funktionalitet för knapparna att 
+	4. I funktionen buttons() hittar du två button taggar. Skriv funktionalitet för knapparna att
 	addera värdet av count med 1 respektive subtrahera värdet med 1
 
-	6. Vid första renderingen av denna vy-komponent 
+	6. Vid första renderingen av denna vy-komponent
 	skall funktionen getCharacterNameFromStarwarsAPI() att anropas
 
 	7. När värdet av count uppdateras skall det ske en ny rendering
 
-	Ifall du lyckats slutföra detta ordentligt skall du nu i applikationen kunna hämta ett nytt karaktärnamn 
+	Ifall du lyckats slutföra detta ordentligt skall du nu i applikationen kunna hämta ett nytt karaktärnamn
 	genom att använda dig av de knappar som finns innuti buttons() funktionen.
 
  */
